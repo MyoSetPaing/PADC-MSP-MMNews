70309ccab30525dd46fce0f97d31b0cccbf5995a
Myo Set Paing <myosetpaing@aceplussolutions.com>
2019-02-18 11:30:54 +0630

    first commit

---
+++ app/src/main/java/com/myosetpaing/padc_msp_mmnews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myosetpaing.padc_msp_mmnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
